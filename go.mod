539d97133866fcd5ab84e54c350b9703cf5fd071
whalyf viana <whalyfeng@gmail.com>
2024-04-26 23:21:41 -0300

    init serpApi and email send

--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/gorilla/handlers v1.5.2 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/serpapi/google-search-results-golang v0.0.0-20240325113416-ec93f510648e // indirect
 )
--- main.go
+++ main.go
@@ -6,13 +6,21 @@ import (
     "net/http"
     "strings"
     "time"
+		"fmt"
+		"os"
 
     "github.com/gorilla/handlers"
     "github.com/gorilla/mux"
+		"github.com/joho/godotenv"
+		// g "github.com/serpapi/google-search-results-golang"
 )
 
 func main() {
     router := mux.NewRouter()
+
+		if err := godotenv.Load(); err != nil {
+			log.Fatalf("Error loading .env file: %v", err)
+		}
 
     // Register the handler for the "/process" endpoint with Gorilla Mux
     router.HandleFunc("/process", handleProcessRequest).Methods("POST")
@@ -38,15 +46,47 @@ func handleProcessRequest(w http.ResponseWriter, r *http.Request) {
     }
 
     processedResults := processNestJSData(searchParams)
+  	// Check if "keywords" exists in processedResults
+		keywords, ok := processedResults["keywords"].(string)
+		if !ok {
+				http.Error(w, "Keywords not found or not a string", http.StatusInternalServerError)
+				return
+		}
+		email, ok := processedResults["email"].(string)
+		if !ok {
+				http.Error(w, "Email not found or not a string", http.StatusInternalServerError)
+				return
+		}
 
+		// Call searchOnGoogle with the keywords
+		results:= searchOnGoogle(keywords, email)
+		fmt.Println(results)
     w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(processedResults)
+    json.NewEncoder(w).Encode(results)
+}
+
+func searchOnGoogle(keywords string, email string) map[string]string{
+	parameter := map[string]string{
+    "api_key": os.Getenv("SERPAPI_KEY"),
+    "engine": "google",
+    "q": keywords,
+    "location": "Brazil",
+    "google_domain": "google.com.br",
+    "gl": "br",
+    "hl": "pt",
+  }
+  // search := g.NewGoogleSearch(parameter, os.Getenv("SERPAPI_KEY"))
+  // results, err := search.GetJSON()
+
+	// fmt.Println(err)
+	return parameter
 }
 
 func processNestJSData(searchParams map[string]interface{}) map[string]interface{} {
     return map[string]interface{}{
         "location":  searchParams["location"],
         "frequency": searchParams["frequency"],
+        "email": searchParams["email"],
         "keywords":  toUpperCase(searchParams["keywords"].(string)),
         "dateTime":  time.Now().Format(time.RFC3339),
         "searchId":   searchParams["searchId"],
